c9e2646e2252ae51508f136d6b20a9e0328478cd
fceresol <42272728+fceresol@users.noreply.github.com>
2022-01-20 16:24:34 +0100

    Add files via upload

---
+++ rhsso-postgresql/build/extensions/postconfigure.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "------------- START UPDATING CONFIGURATION ---------------"
+echo "creating datasources:"
+$JBOSS_HOME/bin/jboss-cli.sh --file=/opt/eap/extensions/datasources.cli
+echo "configuring TX object store:"
+
+local prefix="os${env.JBOSS_NODE_NAME//-/}"
+
+echo "
+embed-server --std-out=echo  --server-config=standalone-openshift.xml
+batch
+/subsystem=transactions:write-attribute(name=use-jdbc-store, value=true)
+/subsystem=transactions:write-attribute(name=jdbc-store-datasource, value=\"java:jboss/datasources/KeycloakDSObjectStore\")
+/subsystem=transactions:write-attribute(name=jdbc-action-store-table-prefix, value=${prefix})
+/subsystem=transactions:write-attribute(name=jdbc-communication-store-table-prefix, value=${prefix})
+/subsystem=transactions:write-attribute(name=jdbc-state-store-table-prefix, value=${prefix})
+run-batch
+
+quit
+" > /opt/eap/extensions/objectstore.cli
+
+$JBOSS_HOME/bin/jboss-cli.sh --file=/opt/eap/extensions/objectstore.cli
+
+echo "configuring x-site-replica"
+
+$JBOSS_HOME/bin/jboss-cli.sh --file=/opt/eap/extensions/x-site-replica.cli
+
+echo "------------- ENDED UPDATING CONFIGURATION ---------------"
